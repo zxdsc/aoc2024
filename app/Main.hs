3687c48190a89483472dfbb2d130cee6e239604c
Boris Potapov <borispotapov@iMac-Anuta.local>
2024-12-04 21:58:51 -0800

    day 2

--- app/Main.hs
+++ app/Main.hs
@@ -6,15 +6,16 @@ import Data.String
 import qualified Data.Text as T
 import Control.Arrow (ArrowChoice(right))
 import GHC.Num (integerFromInt)
+import Data.Function (on)
 
 main :: IO ()
 main = do
    input <- getContents
-   let lists = transform input
-   let res = calc lists
-   print res
-   let res2 = calcSimilarity lists
-   print res2
+   let matrix = transformInput input
+   print $ calcLevels matrix
+   print $ calcLevels' matrix
+
+-- Day 1 
 
 transform :: String -> ([Integer], [Integer])
 transform s = unzip $ map splitLine $ T.splitOn "\n" (T.pack s)
@@ -39,3 +40,40 @@ calcSimilarity (left, right) = sum prod
     where
         individualProd n = n * integerFromInt (length $ filter (== n) right)
         prod = map individualProd left
+
+-- Day 2
+
+transformInput :: String -> [[Integer]]
+transformInput str =
+    let lines = T.splitOn "\n" $ T.pack str
+        list "" = []
+        list x = map (read . T.unpack) $ T.splitOn " " x
+    in map list lines 
+
+
+calcLevels :: [[Integer]] -> Integer
+calcLevels matr = integerFromInt $ length (filter id $ map calcLevel matr)
+
+calcLevels' :: [[Integer]] -> Integer
+calcLevels' matr = integerFromInt $ length (filter id $ map calcLevel' matr)
+
+
+
+calcLevel :: [Integer] -> Bool
+calcLevel [] = False
+calcLevel ls =
+    let difs = zipWith (\pr nxt -> let dist = nxt - pr in (dist, signum dist)) ls (tail ls)
+        inInterval x = x `betweenIncluding` (1, 3)
+    in all (\(dst, sig) -> inInterval (abs dst) && sig == snd (head difs)) difs
+
+calcLevel' :: [Integer] -> Bool
+calcLevel' ls = any calcLevel attemts
+    where
+        attemts = zipWith (++) (inits ls) (tail $ tails ls)
+
+betweenIncluding :: Integer -> (Integer, Integer) -> Bool
+betweenIncluding x (l, r)
+    | x >= l && x <= r = True
+    | otherwise = False
+
+
